1e88c05806c94acffcd7165d1a5a5e8cc8ed0e6e
Rendy Febry <rendyfebry@gmail.com>
2020-05-08 17:45:52 +0700

    Fix NVM Installer

--- node/install.sh
+++ node/install.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# Check for NVM
-if test ! $(which nvm)
-then
+if [ -d "$HOME/.nvm" ]; then
+  echo "  Skipped NVM"
+else
   echo "  Installing NVM for you."
 
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-  NVM_DIR="$HOME/.nvm" 
-  \. $NVM_DIR/nvm.sh
-  \. $NVM_DIR/bash_completion
 fi
 
 exit 0
