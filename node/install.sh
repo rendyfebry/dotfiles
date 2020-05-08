22d9d91629326b33446632da298454e4bbc2084d
Rendy Febry <rendyfebry@gmail.com>
2020-05-08 10:42:31 +0700

    Install NVM

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,15 @@
-if test ! $(which spoof)
+#!/bin/sh
+
+# Check for NVM
+if test ! $(which nvm)
 then
-  if test $(which npm)
-  then
-    sudo npm install spoof -g
-  fi
+  echo "  Installing NVM for you."
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+  NVM_DIR="$HOME/.nvm" 
+  \. $NVM_DIR/nvm.sh
+  \. $NVM_DIR/bash_completion
 fi
+
+exit 0
